be36fa08229e8ce1d5238646c1b751de4e21d0dd
Rafleze <raffaele@20tab.com>
2014-07-10 15:11:34 +0200

    django 1.7 supported

--- cked/widgets.py
+++ cked/widgets.py
@@ -8,8 +8,10 @@ from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
 
 from django.core.exceptions import ImproperlyConfigured
-from django.forms.util import flatatt
-
+try:
+    from django.forms.utils import flatatt
+except ImportError:
+    from django.forms.util import flatatt
 try:
     import json
 except ImportError:
